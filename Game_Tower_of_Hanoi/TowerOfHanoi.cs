dae144e7004580350a3e13f6d3343f9df9b3f053
Rohit Sharma <rohitdropbox25@gmail.com>
2023-10-26 14:33:46 +0300

    Changed a bit of orientation.

--- Game_Tower_of_Hanoi/TowerOfHanoi.cs
+++ Game_Tower_of_Hanoi/TowerOfHanoi.cs
@@ -1,86 +1,88 @@
 using System.Runtime.InteropServices.JavaScript;
 
-namespace Game_Tower_of_Hanoi;
-
-public class TowerOfHanoi
+namespace Game_Tower_of_Hanoi
 {
-    private int moves;
-    private int disks;
-    private List<int> rodA;
-    private List<int> rodB;
-    private List<int> rodC;
-
-    public TowerOfHanoi(int numDisks)
+    public class TowerOfHanoi
     {
-        moves = 0;
-        disks = numDisks;
-        rodA = new List<int>(Enumerable.Range(1, numDisks).Reverse());
-        rodB = new List<int>();
-        rodC = new List<int>();
-    }
-
-    public List<int> GetRodA()
-    {
-        return rodA;
-    }
-
-    public List<int> GetRodB()
-    {
-        return rodB;
-    }
-
-    public List<int> GetRodC()
-    {
-        return rodC;
-    }
-
-    public int GetDisks()
-    {
-        return disks;
-    }
-
-    public void MoveDisk(char sourceRod, char destinationRod)
-    {
-        List<int> source, destination;
-        if (sourceRod == 'A')
-            source = rodA;
-        else if (sourceRod == 'B')
-            source = rodB;
-        else
-            source = rodC;
-
-        if (destinationRod == 'A')
-            destination = rodA;
-        else if (destinationRod == 'B')
-            destination = rodB;
-        else
-            destination = rodC;
-
-        if (source.Count == 0)
+        private int moves;
+        private int disks;
+        private List<int> rodA;
+        private List<int> rodB;
+        private List<int> rodC;
+    
+        public TowerOfHanoi(int numDisks)
         {
-            Console.WriteLine("Invalid move!! Source rot empty.");
-            return;
+            moves = 0;
+            disks = numDisks;
+            rodA = new List<int>(Enumerable.Range(1, numDisks).Reverse());
+            rodB = new List<int>();
+            rodC = new List<int>();
         }
-
-        int diskToMove = source[source.Count - 1];
-
-        if (destination.Count !=0 && diskToMove > destination[destination.Count - 1])
+    
+        public List<int> GetRodA()
         {
-            Console.WriteLine("Invalid move!! Larger disks cannot be placed on top of a smaller disk.");
-            return;
+            return rodA;
         }
-        destination.Add(diskToMove);
-        source.RemoveAt(source.Count - 1);
-        moves++;
-    }
-
-    public bool IsGameWon()
-    {
-        return rodC.Count == disks;
-    }
-
-    public int GetMoves()
-    {
-        return moves;
+    
+        public List<int> GetRodB()
+        {
+            return rodB;
+        }
+    
+        public List<int> GetRodC()
+        {
+            return rodC;
+        }
+    
+        public int GetDisks()
+        {
+            return disks;
+        }
+    
+        public void MoveDisk(char sourceRod, char destinationRod)
+        {
+            List<int> source, destination;
+            if (sourceRod == 'A')
+                source = rodA;
+            else if (sourceRod == 'B')
+                source = rodB;
+            else
+                source = rodC;
+    
+            if (destinationRod == 'A')
+                destination = rodA;
+            else if (destinationRod == 'B')
+                destination = rodB;
+            else
+                destination = rodC;
+    
+            if (source.Count == 0)
+            {
+                Console.WriteLine("Invalid move!! Source rot empty.");
+                return;
+            }
+    
+            int diskToMove = source[source.Count - 1];
+    
+            if (destination.Count !=0 && diskToMove > destination[destination.Count - 1])
+            {
+                Console.WriteLine("Invalid move!! Larger disks cannot be placed on top of a smaller disk.");
+                return;
+            }
+            destination.Add(diskToMove);
+            source.RemoveAt(source.Count - 1);
+            moves++;
+        }
+    
+        public bool IsGameWon()
+        {
+            return rodC.Count == disks;
+        }
+    
+        public int GetMoves()
+        {
+            return moves;
+        }
     }
 }
+
